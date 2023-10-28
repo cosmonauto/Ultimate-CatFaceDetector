c17356d2ee4c43bb5b086bb4b525430509dcbf89
cosmonauto <cosmonautoyu@proton.me>
2023-10-28 21:45:28 +0000

    Changed ondemand video list into checked-list-box type.

--- src/function.cpp
+++ src/function.cpp
@@ -2,4 +2,11 @@
 
 void Function::detectSingleImage( std::string src_path, std::string dst_path )
 {
-  bool show = dst_pa
+  bool show = dst_path.compare("") == 0 ? true : false;
+  std::string imgname = src_path.substr( src_path.find_last_of('/')+1 );
+  std::string write_path = dst_path + "/" + imgname;
+
+  std::cout << "Detection on a single image: " << src_path << std::endl;
+
+  if( show )    std::cout << "Showing detection result" << std::endl;
+  else          std::cout << "Writing detection result to " <<

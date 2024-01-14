7f6b3c409ea9733dc5aa32c76ca261716d32ae68
cosmonauto <cosmonautoyu@proton.me>
2024-01-14 06:58:45 +0000

    ALSA: usb-audio: Fix missing autopm for MIDI input

--- src/main.cpp
+++ src/main.cpp
@@ -11,4 +11,15 @@ void help( char *argv[] );
 int main( int argc, char *argv[] )
 {
   if( argc < 3 )  help( argv );
-  
+  else
+  {
+    string function( argv[1] );
+    string src_path = argv[2];
+    string dst_path="";
+    if( argc > 3 )  dst_path = string( argv[3] );
+
+    if( !function.compare( "image" ) )          Function::detectSingleImage( src_path, dst_path );
+    else if( !function.compare( "images" ) )    Function::detectMultipleImages( src_path, dst_path );
+    else if( !function.compare( "video" ) )
+    {
+     
